e90e075d13a87de44ba178eca498d66611d64a34
Justin Schröder <justin.schroeder@calimoto.com>
2022-03-30 10:54:48 +0200

    use prebuilt cssh

--- Package.swift
+++ Package.swift
@@ -5,18 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "Shout",
-    platforms: [
-        .macOS(.v10_10)
-    ],
     products: [
         .library(name: "Shout", targets: ["Shout"]),
     ],
     dependencies: [
         .package(url: "https://github.com/IBM-Swift/BlueSocket", from: "1.0.46"),
+        .package(url: "https://github.com/JustinGanzer/Libssh2Prebuild.git", from: "1.10.1")
     ],
     targets: [
-        .systemLibrary(name: "CSSH", pkgConfig: "libssh2", providers: [.brew(["libssh2","openssl"])]),
+        //installing via a provider works as well, but unused for now
+        //.systemLibrary(name: "CSSH", pkgConfig: "libssh2", providers: [.brew(["libssh2","openssl"])]),
         .target(name: "Shout", dependencies: ["CSSH", "Socket"]),
         .testTarget(name: "ShoutTests", dependencies: ["Shout"]),
     ]
 )
+
+#if os(watchOS)
+package.targets.removeAll(where: { $0.name == "ShoutTests"})
+#endif
